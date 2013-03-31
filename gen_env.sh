6ed230bb730cb5e89eeb7801d8081edd8f04329e
Chen Yuheng <chyh1990@gmail.com>
2013-03-31 19:09:22 +0800

    python version output in stderr

--- gen_env.sh
+++ gen_env.sh
@@ -8,4 +8,4 @@ arm-eabi-gcc --version|head -1 >>$OUT
 qemu-system-x86_64 --version|head -1 >>$OUT
 emulator-arm -version|head -1 >> $OUT
 gdb --version|head -1 >> $OUT
-python2.6 --version >> $OUT
+python2.6 --version &>> $OUT
